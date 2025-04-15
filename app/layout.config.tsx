d77fa2134848edf1c63821f95a4ae5623433ab90
uplinqhelpcenter <alexander@uplinq.com>
2025-04-15 10:17:34 -0700

    Move homepage from app/(home) to app/page.tsx for Vercel compatibility

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -5,7 +5,7 @@ import { Logo } from './theme';
  * Shared layout configurations
  *
  * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
+ * Home Layout: app/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
