e5c903309ea629172412994399a049e71580e63e
uplinqhelpcenter <alexander@uplinq.com>
2025-04-15 19:39:43 -0700

    Revert "Configure Tailwind with Uplinq brand colors"
    This reverts commit b2636e1db1272abafd13b44ad3b485fc24ecc4e1.

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default withMDX(config);
